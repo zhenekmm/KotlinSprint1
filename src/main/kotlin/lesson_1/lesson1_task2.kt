794bbdd950cff672b6aeb515ed66e13ae73b5be2
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-25 10:46:43 +0300

    KS-1-2 added

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val countOrders: Int = 75
+    val messageGratitude: String = "Спасибо что выбрали нас! Большое спасибо за ваш заказ!"
+
+    println(countOrders)
+    println(messageGratitude)
+
+    var countWorkers = 2000
+//    println(countWorkers)
+    countWorkers = 1999
+    println(countWorkers)
+}
