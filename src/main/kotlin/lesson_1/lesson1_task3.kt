b25bd6fbf099168a247888b34d5a795f04b7b4a9
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-25 11:04:20 +0300

    KS-1-3 create

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+import kotlin.math.min
+
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
+}
