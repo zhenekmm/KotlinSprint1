238e29ce928d88740279e27ef6345173e4ef3214
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-26 10:58:30 +0300

    KS-1-4 READY*

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -7,4 +7,11 @@ fun main() {
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
 }
