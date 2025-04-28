0b8dc8a3158a30a2e1c36e8132d8290eb6180bc8
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-28 17:33:09 +0300

    KS-3-5 READY*

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,7 +2,6 @@ package org.example.lesson_3
 
 fun main() {
     val inputMove = "D2-D4;0".split("-", ";")
-
     val startPoint = inputMove[0]
     val endPoint = inputMove[1]
     val numberMove = inputMove[2]
