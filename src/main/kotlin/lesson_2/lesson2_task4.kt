5f5e638a2ca5bf9d9ea53e12dfe6ec73cfdb2b00
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-28 15:37:00 +0300

    KS-2-4 READY*

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,10 +3,10 @@ package org.example.lesson_2
 fun main() {
     val countCrystallineOre = 7
     val ironOre = 11
-    val gainPercentage = 0.2
+    val gainPercentage = 20
 
-    val crystallineBonus = (countCrystallineOre * gainPercentage).toInt()
-    val ironBonus = (ironOre * gainPercentage).toInt()
+    val crystallineBonus = (countCrystallineOre * gainPercentage / 100).toInt()
+    val ironBonus = (ironOre * gainPercentage / 100).toInt()
 
     println("Количество бонусной кристаллической руды - $crystallineBonus")
     println("Количество бонусной железной руды - $ironBonus")
