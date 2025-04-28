098f9a3f6573f8caf63b3b8cd70827712f33f22e
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-28 15:43:52 +0300

    KS-2-5 READY*

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,9 +4,11 @@ import kotlin.math.pow
 
 fun main() {
     val inputSum = 70000
-    val interestRate = 16.7 / 100
+    val interestRate = 16.7
     val yearCount = 20
+    val countPercent = 100
 
-    val result = inputSum * (1 + interestRate).pow(yearCount)
+
+    val result = inputSum * (1 + interestRate / countPercent).pow(yearCount)
     println(String.format("%.3f", result))
 }
