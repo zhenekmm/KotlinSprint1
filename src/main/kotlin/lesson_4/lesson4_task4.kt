c86798fddba27a0923ecacd243dbc2968d74dcb3
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-29 15:10:05 +0300

    KS-4-4 READY*

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,15 +1,10 @@
 package org.example.lesson_4
 
 fun main() {
-    var numberDay = 5
-    println("""Упражнения для рук:    ${numberDay % 2 == 1}
-Упражнения для ног:    ${numberDay % 2 == 0}
-Упражнения для спины:  ${numberDay % 2 == 0}
-Упражнения для пресса: ${numberDay % 2 == 1}""")
-
-    numberDay++
-    println("""Упражнения для рук:    ${numberDay % 2 == 1}
-Упражнения для ног:    ${numberDay % 2 == 0}
-Упражнения для спины:  ${numberDay % 2 == 0}
-Упражнения для пресса: ${numberDay % 2 == 1}""")
+    val numberDay = 5
+    val isEven = numberDay % 2 == 0
+    println("""Упражнения для рук:    ${!isEven}
+Упражнения для ног:    $isEven
+Упражнения для спины:  $isEven
+Упражнения для пресса: ${!isEven}""")
 }
