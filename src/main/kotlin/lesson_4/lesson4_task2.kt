f14c4ea25f8056b50504eca718283e316d8f7a32
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-29 14:55:59 +0300

    KS-4-2 READY*

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,15 +1,17 @@
 package org.example.lesson_4
 
-fun main() {
-    var weight = 42
-    var volume = 120
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > 35 && weight <= 100) && (volume < 100)}")
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
 
-    weight = 20
-    volume = 80
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > 35 && weight <= 100) && (volume < 100)}")
+fun main() {
+
+    var weight = 20
+    var volume = 80
+    val average = (weight > MIN_WEIGHT && weight <= MAX_WEIGHT) && (volume < MAX_VOLUME)
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': $average")
 
     weight = 50
     volume = 100
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > 35 && weight <= 100) && (volume < 100)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': $average")
 }
