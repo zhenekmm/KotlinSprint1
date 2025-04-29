49fbcb5d600f21e493e306862855fcd190526f6f
Evgeniya <super.maiorowa9@yandex.ru>
2025-04-29 17:27:23 +0300

    KS-4-5 READY*

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,13 +1,17 @@
 package org.example.lesson_4
 
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val MIN_COUNT_BOXES = 50
+
 fun main() {
     val presenceHullDamage = readln().toBoolean()
     val currentCrew = readln().toInt()
     val countBoxes = readln().toInt()
     val favorableWeather = readln().toBoolean()
 
-    val canSail = (!presenceHullDamage && currentCrew >= 55 && currentCrew <= 70 && countBoxes > 50) ||
-            (presenceHullDamage && currentCrew == 70 && favorableWeather && countBoxes >= 50)
+    val canSail = (!presenceHullDamage && currentCrew >= MIN_CREW && currentCrew <= MAX_CREW && countBoxes > MIN_COUNT_BOXES) ||
+            (presenceHullDamage && currentCrew == MAX_CREW && favorableWeather && countBoxes >= MIN_COUNT_BOXES)
 
     println("Корабль может отправиться в плавание? $canSail")
 }
