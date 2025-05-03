6d9d3babc437590f13939dabe1ba206d2ffb2664
= <il-kosicin@mail.ru>
2025-05-03 12:45:02 +0300

    KS-5-3 READY**

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -10,8 +10,10 @@ fun main() {
     val inputB = readln().toInt()
 
 
-    if ((inputA == firstNumber && inputB == secondNumber) || (inputB == firstNumber && inputA == secondNumber)) println("Поздравляем! Вы выиграли главный приз!")
-    else if(inputA == firstNumber || inputA == secondNumber || inputB == secondNumber || inputB == firstNumber) println("Вы выиграли утешительный приз!")
+    if ((inputA == firstNumber && inputB == secondNumber) || (inputB == firstNumber && inputA == secondNumber))
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if(inputA == firstNumber || inputA == secondNumber || inputB == secondNumber || inputB == firstNumber)
+        println("Вы выиграли утешительный приз!")
     else println("Неудача!")
 
     println("Правильные числа для победы: $firstNumber и $secondNumber")
